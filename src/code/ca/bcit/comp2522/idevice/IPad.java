2f60233e0a9cc46fccae2b9ec43452629ad5d04f
Nathan Oloresisimo <146795346+NateRolo@users.noreply.github.com>
2025-01-25 21:36:40 -0800

    feat: added toString(), equals(), hashCode(), instance variables, getters and setters, and validation methods.

--- src/code/ca/bcit/comp2522/idevice/IPad.java
+++ src/code/ca/bcit/comp2522/idevice/IPad.java
@@ -4,8 +4,9 @@ public class IPad extends IDevice
 {
 
     private final String purpose;
+
     private boolean hasACase;
-    private final String iOSVersion;
+    private String iOSVersion;
 
     public IPad(final boolean hasACase,
                 final String iOSVersion)
@@ -27,11 +28,11 @@ public class IPad extends IDevice
      *
      * @param iOSVersion the iOS version to validate as a String.
      */
-    private static void validateIOSVersion(final String iOSVersion) {
+    private static void validateIOSVersion(final String iOSVersion)
+    {
         if (iOSVersion == null || iOSVersion.isEmpty()) {
             throw new IllegalArgumentException("iOS version cannot be null or empty.");
         }
-
 
         final String iosVersionRegex = "^\\d+\\.\\d+(\\.\\d+)?$";
 
@@ -42,6 +43,88 @@ public class IPad extends IDevice
                                                "where X, Y, and Z are integers.");
         }
 
+    }
 
+    public String getIOSVersion()
+    {
+        return iOSVersion;
+    }
 
+    public void setIOSVersion(final String iOSVersion)
+    {
+        validateIOSVersion(iOSVersion);
+
+        this.iOSVersion = iOSVersion;
+    }
+
+    public boolean iPadHasACase()
+    {
+        return hasACase;
+    }
+
+    public void setHasACase(final boolean hasACase)
+    {
+        this.hasACase = hasACase;
+    }
+
+    @Override
+    public String toString()
+    {
+        final StringBuilder builder;
+        final String        detailsAsString;
+        final String deviceName;
+
+        deviceName = this.getClass().getSimpleName();
+
+        builder = new StringBuilder();
+
+        builder.append("Device: ")
+               .append(deviceName)
+               .append("\n")
+               .append("Purpose: ")
+               .append(this.getPurpose())
+               .append("\n")
+               .append("Has a case: ")
+               .append(hasACase)
+               .append("\n")
+               .append("iOS Version: ")
+               .append(iOSVersion);
+
+        detailsAsString = builder.toString();
+
+        return detailsAsString;
+
+    }
+
+    @Override
+    void printDetails()
+    {
+        final String details;
+
+        details = this.toString();
+
+        System.out.println(details);
+    }
+
+    @Override
+    public boolean equals(final Object o)
+    {
+        if(o == null)
+        {
+            return false;
+        }
+
+        if(!(o.getClass().equals(this.getClass())))
+        {
+            return false;
+        }
+
+        return ((IPad) o).getIOSVersion().equals(this.getIOSVersion());
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return super.hashCode();
+    }
 }
