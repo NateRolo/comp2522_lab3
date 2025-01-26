8bb86199c57ef2aff437872d433e8c7428238c64
Nathan Oloresisimo <146795346+NateRolo@users.noreply.github.com>
2025-01-25 22:52:45 -0800

    feat: wrote constructor, memory validation method, and memory size constants.

--- src/code/ca/bcit/comp2522/idevice/IPhone16.java
+++ src/code/ca/bcit/comp2522/idevice/IPhone16.java
@@ -1,6 +1,44 @@
 package ca.bcit.comp2522.idevice;
 
-public class IPhone16
+public class IPhone16 extends IPhone
 {
+    private static final int LOW_TIER_MEMORY_SIZE_GB = 128;
+    private static final int MID_TIER_MEMORY_SIZE_GB = 256;
+    private static final int HIGH_TIER_MEMORY_SIZE_GB = 512;
 
+    private final boolean hasHighResCamera;
+    private final int     memorySizeGB;
+
+    /**
+     * Constructs an {@code IPhone16} object.
+     *
+     * @param phonePlanMinutes the initial number of minutes in the phone plan.
+     * @param carrier          the carrier providing the phone plan.
+     */
+    public IPhone16(final double phonePlanMinutes,
+                    final String carrier,
+                    final boolean hasHighResCamera,
+                    final int memorySizeGB)
+    {
+        super(phonePlanMinutes,
+              carrier);
+
+        validateMemorySize(memorySizeGB);
+
+        this.hasHighResCamera = hasHighResCamera;
+        this.memorySizeGB = memorySizeGB;
+    }
+
+    private static void validateMemorySize(final int memorySizeGB)
+    {
+        if((memorySizeGB == LOW_TIER_MEMORY_SIZE_GB) ||
+           (memorySizeGB == MID_TIER_MEMORY_SIZE_GB) ||
+           (memorySizeGB == HIGH_TIER_MEMORY_SIZE_GB))
+        {
+            return;
+        }
+
+        throw new IllegalArgumentException("Invalid memory size: " +
+                                           memorySizeGB);
+    }
 }
