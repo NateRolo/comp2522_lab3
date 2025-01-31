91fa92e8178aa546924d5f4fd22d8f738b774224
JFonseca-BC <jfonseca10@my.bcit.ca>
2025-01-31 10:41:51 -0800

    Finished javadoc for IDevice class comment

--- src/code/ca/bcit/comp2522/idevice/IDevice.java
+++ src/code/ca/bcit/comp2522/idevice/IDevice.java
@@ -1,6 +1,9 @@
 package ca.bcit.comp2522.idevice;
 
 /**
+ * Defines shared properties of ©Apple® iDevices. Provides
+ * method to get the purpose specific to the device and
+ * an abstract method to return the details of the device.
  *
  * @author Nathan O
  */
